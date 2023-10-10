913638f12a3c56440732630694104836e48cf774
James Ngandu <jamesmngandu@gmail.com>
2023-10-11 00:06:35 +0300

    commit

---
+++ the c++/datatype/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        string characterName = "james";
+        int characterAge;
+        characterAge = 20;
+    cout<<"Hello "<<characterName<<endl;
+    cout << "He is "<<characterAge<<" years old"<<endl;
+
+    return(0);
+}
