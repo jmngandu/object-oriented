218f409041f60b2175ba733eb43b19b3bb4752c4
James Ngandu <jamesmngandu@gmail.com>
2023-10-11 19:48:00 +0300

    added

--- the c++/datatype/main.cpp
+++ the c++/datatype/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -6,11 +7,18 @@ int main(){
         int characterAge = 20;
         char grade = 'A';
         float gpa = 4.0;
+        double num = 6.8998;
+        bool isEven = true;
+
+
         
         
     cout<<"Hello "<<characterName<<endl;
     cout << characterName<<" is "<<characterAge<<" years old"<<endl;
     cout<<characterName<<" scored "<<grade<< "and my gpa is " <<gpa<<endl;
+    cout<<characterName.length()<<endl;
+    cout<<characterName[3]<<endl;
+    cout<<pow(2,5)<<endl;
 
     return(0);
 }
