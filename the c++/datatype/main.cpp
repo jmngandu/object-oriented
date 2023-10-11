8503f2258dbcbe3e90466aee41370a9df70c0c69
James Ngandu <jamesmngandu@gmail.com>
2023-10-11 15:14:10 +0300

    data types

--- the c++/datatype/main.cpp
+++ the c++/datatype/main.cpp
@@ -5,10 +5,12 @@ int main(){
         string characterName = "james mukuvi ngandu";
         int characterAge = 20;
         char grade = 'A';
+        float gpa = 4.0;
+        
         
     cout<<"Hello "<<characterName<<endl;
     cout << characterName<<" is "<<characterAge<<" years old"<<endl;
-    cout<<characterName<<" scored "<<grade<<endl;
+    cout<<characterName<<" scored "<<grade<< "and my gpa is " <<gpa<<endl;
 
     return(0);
 }
