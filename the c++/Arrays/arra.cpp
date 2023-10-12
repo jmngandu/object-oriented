895dfc411eb425f0e8c5a6db9bc8dbb6284e697d
James Ngandu <jamesmngandu@gmail.com>
2023-10-12 13:01:57 +0300

    arrays

---
+++ the c++/Arrays/arra.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int luckyNumbers[] = {1, 2, 4, 5, 6, 56, 9};
+    int funNumbers[20];//assigning arrays
+    funNumbers[3] = 20;
+    cout<<funNumbers[3]<<endl;
+    cout<<luckyNumbers[0]<<endl;
+    return(0);
+}
