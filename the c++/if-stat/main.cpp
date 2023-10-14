e6f9b66dbc7988293c7d17c205c7134629ebc85e
James Ngandu <jamesmngandu@gmail.com>
2023-10-14 22:45:58 +0300

    if-statements

---
+++ the c++/if-stat/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+        bool isMale = true;
+        bool isTall = false;
+        if(isMale && isTall){
+            cout<<"Your a male"<<endl;
+        }else{
+            cout<<"Your a female"<<endl;
+        }
+
+    return 0;
+}
