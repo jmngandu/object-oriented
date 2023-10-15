0a277c723b6b192950b05f3196e3d3e165eb2667
James Ngandu <jamesmngandu@gmail.com>
2023-10-15 17:01:26 +0300

    while ; do-while

---
+++ the c++/while/do-wh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 1;
+    string name = "james";
+
+    do{
+        cout<<"hello "<<name<<endl;
+        num++;
+    }while(num < 5);
+
+    return 0;
+}
---
+++ the c++/while/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 0;
+    while(num < 10){
+        cout<<num<<endl;
+        num++;
+    }
+    return 0;
+}
