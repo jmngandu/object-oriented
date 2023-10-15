3b4704acb42e4c95b8f440f7ec436887c35364a3
James Ngandu <jamesmngandu@gmail.com>
2023-10-15 17:05:45 +0300

    guessing game

---
+++ the c++/Guessing Game/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    cout<<"hello"<<endl;
+    printf("Hello\n");
+     printf("Hello\n");
+      printf("Hello\n");
+       printf("Hello\n");
+        printf("Hello\n");
+         printf("Hello\n");
+          printf("Hello\n");
+           printf("Hello\n");
+            printf("Hello\n");
+
+
+    return 0;
+}
