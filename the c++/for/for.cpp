71c9ca4c8d7e0ffd1c89277883b8f9deb64455c2
James Ngandu <jamesmngandu@gmail.com>
2023-10-15 19:02:13 +0300

    for - loop

---
+++ the c++/for/for.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int nums[] = {2, 3, 4, 7, 8, 9, 1 ,4, 3, 5};
+    for(int i = 0; i < 8; i++){
+        cout<<nums[i]<<endl;
+    }
+    return 0;
+}
