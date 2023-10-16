df1c734a7d0df207d9f666a0411fe340d38e5ec5
James Ngandu <jamesmngandu@gmail.com>
2023-10-16 23:26:11 +0300

    for - loop

--- the c++/Arrays/2dArray.cpp
+++ the c++/Arrays/2dArray.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
 
+    int numberGrind[3] [2] = {
+        { 2, 3},
+        { 4, 6},
+        { 5, 8}
+    };
+
+    cout<<numberGrind[2][1]<<endl;
+
     return 0;
 }
