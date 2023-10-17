8d2723a0d0086021855c19da6aaed29121ebf148
James Ngandu <jamesmngandu@gmail.com>
2023-10-17 07:13:32 +0300

    pointers

---
+++ the c++/pointers/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      
+      int num = 3;
+      double gpa = 4.5;
+      string name = "james";
+      int *Pnum = &num;
+      cout<<*Pnum<<endl;
+    return 0;
+}
