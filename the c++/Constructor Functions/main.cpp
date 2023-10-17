d74d884cdc1c0f45420ba9d2486352b1262bfa6f
James Ngandu <jamesmngandu@gmail.com>
2023-10-17 08:00:16 +0300

    Constructor Functions

---
+++ the c++/Constructor Functions/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+    public:
+    string name;
+    string school;
+    int age;
+    float gpa;
+    Person(string Aname, string Aschool, int Aage, float Agpa){
+        name = Aname;
+        school = Aschool;
+        age = Aage;
+        gpa = Agpa;
+        }
+};
+
+int main(){
+    Person Person1("James","University of Eldoret",20, 4.5);
+    Person Person2("John", "University of Nairobi", 22, 4.0);
+    cout<<Person1.name;
+
+
+
+    return 0;
+}
